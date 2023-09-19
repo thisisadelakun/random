c8d1b58e6d1813c98135cf7010233ba403a787e7
Adelakun Abayomi <adelakunabayomi123@gmail.com>
2023-09-19 14:54:49 +0100

    mi

--- src/components/library/Library.jsx
+++ src/components/library/Library.jsx
@@ -1,11 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import './Library.css'
+import './Library.css';
 
-// import models
+// Import models
 import { libraryPage, myLibrary } from '../../models/db/db2';
 import LibraryTiles from '../../models/libararyfolder/LibraryTiles';
 import SEO from '../../models/seo/SEO';
+
+// Import the smoothscroll-anchor-polyfill
+import smoothscrollPolyfill from 'smoothscroll-polyfill';
+import smoothscrollAnchorPolyfill from 'smoothscroll-anchor-polyfill';
+
+// Initialize the smoothscroll polyfills
+smoothscrollPolyfill.polyfill();
+smoothscrollAnchorPolyfill.polyfill();
 
 const ITEMS_PER_PAGE = 10;
 
@@ -14,6 +22,7 @@ const Library = () => {
     const totalPages = Math.ceil(myLibrary.length / ITEMS_PER_PAGE);
 
     const handlePageClick = (pageNumber) => {
+        // Use the smooth scroll behavior for anchor links
         window.scrollTo({
             top: 0,
             behavior: 'smooth',
@@ -43,12 +52,12 @@ const Library = () => {
     const displayedLibrary = myLibrary.slice(startIndex, endIndex);
 
     return (
-        <div className='library-col'>
+        <div className="library-col">
             <SEO
                 title="Blog | Adelakun's Portfolio"
                 description="Portfolio's Blog Page"
                 name="Web Developer Portfolio"
-                type='Portfolio'
+                type="Portfolio"
             />
             <div className="library-page containers">
                 <div className="lib-page-header">
@@ -56,12 +65,10 @@ const Library = () => {
                     <p>{libraryPage.subTitle}</p>
                 </div>
                 <LibraryTiles displayedLibrary={displayedLibrary} />
-                <div className="pagination">
-                    {getPageNumbers()}
-                </div>
+                <div className="pagination">{getPageNumbers()}</div>
             </div>
         </div>
     );
-}
+};
 
 export default Library;
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'smoothscroll-polyfill';
+import smoothscrollPolyfill from 'smoothscroll-polyfill';
 
 // Initialize the polyfill
-window.__forceSmoothScrollPolyfill__ = true;
+smoothscrollPolyfill.polyfill();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
